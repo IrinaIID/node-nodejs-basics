3a632ee02eae1dccbe44f72de28a0e55d5b8f04e
Iryna Ishchanka <28myhome@gmail.com>
2023-06-17 09:15:57 +0300

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  fs.stat(path.resolve(__dirname, './files', 'fileToRemove.txt'), (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.unlink(path.resolve(__dirname, './files', 'fileToRemove.txt'), err => {
+        if(err) throw err;
+      });
+    }
+  });
 };
 
 await remove();
