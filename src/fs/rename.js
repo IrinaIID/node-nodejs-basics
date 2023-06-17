c90fd250f929a6820789ca3bfab06009de80f3b6
Iryna Ishchanka <28myhome@gmail.com>
2023-06-17 09:03:11 +0300

    feat: add rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  fs.stat(path.resolve(__dirname, './files', 'wrongFilename.txt'), (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.rename(
+        path.resolve(__dirname, './files', 'wrongFilename.txt'),
+        path.resolve(__dirname, './files', 'properFilename.md'),
+        err => {
+          if(err) throw err;
+        }
+      )
+    }
+  });
 };
 
 await rename();
