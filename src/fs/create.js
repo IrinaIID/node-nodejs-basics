b96d9da0c4163ce548f6398a5d813244228c2260
Iryna Ishchanka <28myhome@gmail.com>
2023-06-17 08:54:07 +0300

    feat: add copy.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -9,9 +9,9 @@ const __dirname = path.dirname(__filename);
 const create = async () => {
   fs.stat(path.resolve(__dirname, './files', 'fresh.txt'), (err) => {
     if (!err) {
-      throw new Error('FS operation failed')
+      throw new Error('FS operation failed');
     } else {
-      fsPromises.writeFile(path.resolve(__dirname, './files', 'fresh.txt'), 'I am fresh and young')
+      fsPromises.writeFile(path.resolve(__dirname, './files', 'fresh.txt'), 'I am fresh and young');
     }
   });
 };
