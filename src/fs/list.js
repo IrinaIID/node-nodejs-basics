2c831338847350683cb3e8405fd03f8700eae7fe
Iryna Ishchanka <28myhome@gmail.com>
2023-06-18 13:01:47 +0300

    feat: add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  fs.stat(path.resolve(__dirname, 'files'), (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+        if(err) throw err;
+        files.forEach(doc => {
+          console.log(doc);
+        });
+      })
+    }
+  });
 };
 
 await list();
