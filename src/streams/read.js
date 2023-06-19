06d2a0cecd628fab5010055a985eb1c254e7cde6
Iryna Ishchanka <28myhome@gmail.com>
2023-06-19 13:10:33 +0300

    feat: add streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const { stdout } = process;
+  const readebleStream = fs.createReadStream(path.resolve(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
+  readebleStream.on('data', chunk => {
+    stdout.write(chunk);
+  });
 };
 
 await read();
