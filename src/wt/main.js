1b4ff52f82f800fce12438cf2e0c7c948b256c7a
Iryna Ishchanka <28myhome@gmail.com>
2023-06-20 00:19:08 +0300

    feat: add wt folder

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,35 @@
-import { Worker, workerData } from 'worker_threads';
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { rejects } from 'assert';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const performCalculations = async () => {
-  const worker = new Worker('./worker.js', {
-    workerData: 2
+  const numOfProcessorCore = os.cpus().length;
+
+  const funcCalculation = (workerData) => new Promise((resolve) => {
+    const worker = new Worker(path.resolve(__dirname, './worker.js'), { workerData });
+    worker.on('message', (message) => resolve( {
+      status: 'resolved',
+      data: message
+    }));
+    worker.on('error', (err) => resolve( {
+      status: 'error',
+      data: null
+    }));
   });
-    // Write your code here
-    // console.log('ggg')
+
+  const arrNumCores = [];
+
+  for (let i = 0; i < numOfProcessorCore; i++) {
+    arrNumCores.push(funcCalculation(i + 10));
+  }
+
+  const result = await Promise.all(arrNumCores);
+  console.log(result);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,10 +1,10 @@
-import { workerData } from 'worker_threads';
+import { parentPort, workerData } from 'worker_threads';
 
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  nthFibonacci(workerData);
+  parentPort.postMessage(nthFibonacci(workerData));
     // This function sends result of nthFibonacci computations to main thread
 };
 
